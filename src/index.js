2d6937f8dd6d4402ade518d0be0619dae18f053a
Igor Khudik <js.web23@gmail.com>
2016-05-21 20:38:41 +0300

    Init SearchBar class

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/searchBar';
+
 const API_KEY = 'AIzaSyAJSS28HZ7w7dkQTs5flYCu8PzqNqb_Pu4';
 
 const App = () => {
-    return <div>Hi!</div>
+    return (
+        <div>
+            <SearchBar/>
+        </div>
+    );
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
